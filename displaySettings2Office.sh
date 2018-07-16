ffac5e7c844e0ac0b58a1b2014f9db98b824c25d
Thomas Vogel <toms.vogel@gmail.com>
2018-07-16 14:14:35 +0200

    changes on keyboards

---
+++ displaySettings2Office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --off --output DP-2-8 --primary --mode 1920x1200 --pos 2560x0 --rotate normal --output eDP-1 --off --output DP-1-1 --off --output DP-1-8 --off --output DP-2 --off --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal
---
+++ keyboard.sh
@@ -0,0 +1,9 @@
+setxkbmap -option 
+setxkbmap -layout de -variant mac 
+
+setxkbmap -option caps:escape
+setxkbmap -option altwin:swap_lalt_lwin
+setxkbmap -option lv3:switch
+
+# xmodmap -e 'keycode 49 = less greater less greater bar brokenbar bar' -e 'keycode 94 = dead_circumflex degree dead_circumflex degree U2032 U2033 U2032'
+
