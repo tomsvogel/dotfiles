7a6b639f24cc5cf2d9b5232f50719fd64b09bf8b
Thomas Vogel <toms.vogel@gmail.com>
2018-08-02 08:58:42 +0200

    linux changes

--- displaySettings1.sh
+++ displaySettings1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-2-1 --off --output DP-2-8 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off --output DP-1-8 --off --output DP-2 --off --output DP-1 --off
+xrandr --output DP-2-1 --off --output DP-2-8 --off --output eDP-1 --primary --mode 3200x1800 --pos 0x0 --rotate normal --output DP-1-1 --off --output DP-1-8 --off --output DP-2 --off --output DP-1 --off
