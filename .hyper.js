a5adcf2a83535c8b6b85882feb113d77ff08e10e
Thomas Vogel <thomas@arkulpa.at>
2018-08-13 13:43:05 +0200

    changes on hyper

--- .hyper.js
+++ .hyper.js
@@ -5,22 +5,22 @@ module.exports = {
 
     // font family with optional fallbacks
     fontFamily:
-      '"Roboto Mono", "Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+      '"Meslo LG M DZ for Powerline","Roboto Mono", "Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: "rgba(248,28,229,0.8)",
+    cursorColor: 'rgba(248,28,229,0.8)',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: "BLOCK",
+    cursorShape: 'BLOCK',
 
     // color of the text
-    foregroundColor: "#fff",
+    foregroundColor: '#fff',
 
     // terminal background color
-    backgroundColor: "#000",
+    backgroundColor: '#000',
 
     // border color (window, tabs)
-    borderColor: "#333",
+    borderColor: '#000',
 
     // custom css to embed in the main window
     css: `
@@ -40,59 +40,59 @@ module.exports = {
     `,
 
     // custom css to embed in the terminal window
-    termCSS: "",
+    termCSS: '',
 
     // set to `true` if you're using a Linux set up
     // that doesn't shows native menus
     // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: "",
+    showHamburgerMenu: '',
 
     // set to `false` if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: "",
+    showWindowControls: 'false',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: "12px 14px",
+    padding: '5px 5px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: "#000000",
-      red: "#ff0000",
-      green: "#33ff00",
-      yellow: "#ffff00",
-      blue: "#0066ff",
-      magenta: "#cc00ff",
-      cyan: "#00ffff",
-      white: "#d0d0d0",
-      lightBlack: "#808080",
-      lightRed: "#ff0000",
-      lightGreen: "#33ff00",
-      lightYellow: "#ffff00",
-      lightBlue: "#0066ff",
-      lightMagenta: "#cc00ff",
-      lightCyan: "#00ffff",
-      lightWhite: "#ffffff"
+      black: '#000000',
+      red: '#ff0000',
+      green: '#33ff00',
+      yellow: '#ffff00',
+      blue: '#0066ff',
+      magenta: '#cc00ff',
+      cyan: '#00ffff',
+      white: '#d0d0d0',
+      lightBlack: '#808080',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#ffffff',
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
-    shell: "",
+    shell: '',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
-    shellArgs: ["--login"],
+    shellArgs: ['--login'],
 
     // for environment variables
     env: {},
 
     // set to false for no bell
-    bell: "SOUND",
+    bell: 'SOUND',
 
     // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false
+    copyOnSelect: false,
 
     // URL to custom bell
     // bellSoundURL: 'http://example.com/bell.mp3',
@@ -106,10 +106,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-hide-tabs", "hyper-dark-scrollbar", "hyper-website-theme"],
+  plugins: ['hyper-hide-tabs', 'hyper-dark-scrollbar', 'hyper-website-theme', 'hyper-hide-title', 'hyperminimal'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  localPlugins: [],
 };
