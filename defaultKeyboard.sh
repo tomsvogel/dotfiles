5a8f0d818bbd31e5d7b497fca89f34aab0b9185a
Thomas Vogel <toms.vogel@gmail.com>
2019-02-11 10:32:54 +0100

    adds settings for external screen only

--- defaultKeyboard.sh
+++ defaultKeyboard.sh
@@ -1,11 +1,1 @@
-setxkbmap -option 
-# setxkbmap -layout de -variant nodeadkeys 
-setxkbmap -layout de
-
-setxkbmap -option caps:escape
-setxkbmap -option altwin:swap_alt_win
-setxkbmap -option ctrl:rctrl_ralt
-# setxkbmap -option lv3:switch
-
-#xmodmap -e 'keycode 49 = less greater less greater bar brokenbar bar' -e 'keycode 94 = dead_circumflex degree dead_circumflex degree U2032 U2033 U2032'
-
+setxkbmap -layout de -variant mac_nodeadkeys -model mac -option caps:escape -option altwin:swap_lalt_lwin
--- macKeyboard.sh
+++ macKeyboard.sh
@@ -1,8 +1,3 @@
-setxkbmap -option 
-setxkbmap -layout de -variant mac 
-
-setxkbmap -option caps:escape
-#setxkbmap -option altwin:swap_alt_win
-
-xmodmap -e 'keycode 49 = less greater less greater bar brokenbar bar' -e 'keycode 94 = dead_circumflex degree dead_circumflex degree U2032 U2033 U2032'
-
+setxkbmap -option
+setxkbmap -layout de -variant mac_nodeadkeys -model mac -option caps:escape
+# xmodmap -e 'keycode 49 = less greater less greater bar brokenbar bar' -e 'keycode 94 = dead_circumflex degree dead_circumflex degree U2032 U2033 U2032'
