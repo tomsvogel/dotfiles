4548a1e4df82263ff3e8bc5463b3d08cf6dbc51d
Thomas Vogel <toms.vogel@gmail.com>
2019-05-06 13:35:13 +0200

    changes on screen and hyper conf

--- .hyper.js
+++ .hyper.js
@@ -1,10 +1,10 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 13,
 
     // font family with optional fallbacks
-    fontFamily: 'Hack, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'Hack,"DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
